b5fe21a83317ec305dbad1db5802fe929117901b
Michael Huynh <mike.huynh@gmail.com>
2022-01-17 11:15:28 -0500

    Add .setup-vps.sh script.

---
+++ .setup-vps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#  Sets up a remote server with common tools.
+set -Eeuo pipefail
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install git tmux vim htop mosh
+
+sudo apt install zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ssh-keygen -t ed25519 -C "mikexstudios"
